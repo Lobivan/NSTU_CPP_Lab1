71c89072498a4687ea62761f8986ebba6f2e07ba
lobivan <tloraq@student.21-school.ru>
2023-11-29 14:12:13 +0700

    added append method

--- src/MyString.hpp
+++ src/MyString.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <cstring>
 #include <iostream>
 
@@ -43,17 +44,58 @@ class MyString {
     data = nullptr;
   }
 
-  size_t size() { return curSize; }
+  size_t size() const { return curSize; }
 
-  size_t max_size() { return maxSize; }
+  size_t max_size() const { return maxSize; }
 
-  void print() {
+  void resize(size_t n) {
+    if (n != max_size()) {
+      char *new_data = new char[n]();
+      curSize = std::min(n, size());
+      maxSize = n;
+      for (int i = 0; i < curSize; i++) {
+        new_data[i] = data[i];
+      }
+      delete[] data;
+      data = new_data;
+    }
+  }
+
+  MyString append(char *arr) {
+    size_t arrlen = strlen(arr);
+    while (size() + arrlen > max_size()) {
+      resize(max_size() * 2);
+    }
+    for (int i = 0; i < arrlen; i++) {
+      data[size() + i] = arr[i];
+    }
+    curSize += arrlen;
+    return *this;
+  }
+
+  MyString append(const MyString &str) {
+    char *tmp = str.to_char();
+    append(tmp);
+    delete[] tmp;
+    return *this;
+  }
+
+  char *to_char() const {
+    char *res = new char[size() + 1]();
+    for (int i = 0; i < size(); i++) {
+      res[i] = data[i];
+    }
+    res[size()] = '\0';
+    return res;
+  }
+
+  void print() const {
     for (int i = 0; i < curSize; i++) {
       std::cout << data[i];
     };
   }
 
-  void print_info() {
+  void print_info() const {
     std::cout << "размер строки: " << size() << "\n";
     std::cout << "максимальный размер строки: " << max_size() << "\n";
     std::cout << "строка: \'";
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
 #include "MyString.hpp"
 
 int main(void) {
-  std::cout << "\n Демонстрация работы конструктора по умолчанию \n";
+  std::cout << "\n Демонстрация работы конструктора по умолчанию: \n";
   MyString str1;
   str1.print_info();
 
-  std::cout << "\n Демонстрация работы конструктора с параметром размера \n";
+  std::cout << "\n Демонстрация работы конструктора с параметром размера: \n";
   MyString str2(4);
   str2.print_info();
 
-  std::cout
-      << "\n Демонстрация работы конструктора с параметром массива символов \n";
+  std::cout << "\n Демонстрация работы конструктора с параметром массива "
+               "символов: \n";
   size_t arrlen = 9;
   char *arr = new char[arrlen];
   for (int i = 0; i < arrlen - 1; i++) {
@@ -19,8 +19,21 @@ int main(void) {
   MyString str3(arr);
   str3.print_info();
 
-  std::cout << "\n Демонстрация работы конструктора копирования \n";
+  std::cout << "\n Демонстрация работы конструктора копирования: \n";
   MyString str4(str3);
+  str4.print_info();
+
+  std::cout << "\n Демонстрация работы метода изменения размера: \n";
+  str4.resize(5);
+  str4.print_info();
+  str4.resize(11);
+  str4.print_info();
+
+  std::cout << "\n Демонстрация работы метода добавления массива символов в "
+               "конец строки: \n";
+  str4.append(arr);
+  str4.print_info();
+  str4.append(str4);
   str4.print_info();
 
   delete[] arr;
